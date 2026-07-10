ce5923e8ee33281311fc826f5b624c49936ed490
Vlad <vladshoshin19@gmail.com>
2026-07-10 21:33:27 +0700

    import export modules

--- dist/bundle.js
+++ dist/bundle.js
@@ -27,7 +27,7 @@ eval("{__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-ext
 (__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _css_style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./css/style.css */ \"./src/css/style.css\");\n/* harmony import */ var _js_app__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./js/app */ \"./src/js/app.js\");\n/* harmony import */ var _js_app__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_js_app__WEBPACK_IMPORTED_MODULE_1__);\n\r\n\n\n//# sourceURL=webpack://webpack-project/./src/index.js?\n}");
+eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _css_style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./css/style.css */ \"./src/css/style.css\");\n/* harmony import */ var _js_app__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./js/app */ \"./src/js/app.js\");\n\r\n\n\n//# sourceURL=webpack://webpack-project/./src/index.js?\n}");
 
 /***/ },
 
@@ -35,9 +35,31 @@ eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _cs
 /*!***********************!*\
   !*** ./src/js/app.js ***!
   \***********************/
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _game__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./game */ \"./src/js/game.js\");\n\r\n\r\nconst game = new _game__WEBPACK_IMPORTED_MODULE_0__[\"default\"]();\r\ngame.start();\n\n//# sourceURL=webpack://webpack-project/./src/js/app.js?\n}");
+
+/***/ },
+
+/***/ "./src/js/domain.js"
+/*!**************************!*\
+  !*** ./src/js/domain.js ***!
+  \**************************/
 () {
 
-eval("{console.log('app worked')\n\n//# sourceURL=webpack://webpack-project/./src/js/app.js?\n}");
+eval("{class Character {\r\n}\n\n//# sourceURL=webpack://webpack-project/./src/js/domain.js?\n}");
+
+/***/ },
+
+/***/ "./src/js/game.js"
+/*!************************!*\
+  !*** ./src/js/game.js ***!
+  \************************/
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   GameSavingData: () => (/* binding */ GameSavingData),\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__),\n/* harmony export */   readGameSaving: () => (/* binding */ readGameSaving),\n/* harmony export */   writeGameSaving: () => (/* binding */ writeGameSaving)\n/* harmony export */ });\n/* harmony import */ var _domain__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./domain */ \"./src/js/domain.js\");\n/* harmony import */ var _domain__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_domain__WEBPACK_IMPORTED_MODULE_0__);\n\r\n\r\nclass Game {\r\n  start() {\r\n    console.log('game started');\r\n  }\r\n}\r\n\r\nclass GameSavingData {\r\n}\r\n\r\nfunction readGameSaving() {\r\n}\r\n\r\nfunction writeGameSaving() {\r\n}\r\n\r\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Game);\r\n\n\n//# sourceURL=webpack://webpack-project/./src/js/game.js?\n}");
 
 /***/ }
 
